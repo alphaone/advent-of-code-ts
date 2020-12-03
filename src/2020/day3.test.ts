38585128732cb30acfb85cc9219fb72e9730a7f0
Torsten Mangner <tmangner@inoxio.de>
2020-12-03 18:05:54 +0100

    Day 3 Part 2

--- src/2020/day3.test.ts
+++ src/2020/day3.test.ts
@@ -1,4 +1,4 @@
-import { treesOnSlope } from './day3'
+import { Slope, treesOnSlope } from './day3'
 import { readInput } from '../utils'
 
 const exampleMap = [
@@ -21,5 +21,34 @@ test('solve puzzle a', () => {
 })
 
 test('count trees on slope in example', () => {
-  expect(treesOnSlope(0, [3, 1], exampleMap)).toBe(7)
+  expect(treesOnSlope(0, [3, 1], [...exampleMap])).toBe(7)
+})
+
+test('multiple slopes', () => {
+  const res = [
+    <Slope>[1, 1],
+    <Slope>[3, 1],
+    <Slope>[5, 1],
+    <Slope>[7, 1],
+    <Slope>[1, 2],
+  ].map((slope) => {
+    return treesOnSlope(0, slope, [...exampleMap])
+  })
+  expect(res).toEqual([2, 7, 3, 4, 2])
+  expect(res.reduce((acc, cur) => acc * cur)).toEqual(336)
+})
+
+test('solve puzzle b', () => {
+  const input = readInput('src/2020/day3.txt')
+  const res = [
+    <Slope>[1, 1],
+    <Slope>[3, 1],
+    <Slope>[5, 1],
+    <Slope>[7, 1],
+    <Slope>[1, 2],
+  ].map((slope) => {
+    return treesOnSlope(0, slope, [...input])
+  })
+  expect(res).toEqual([84, 228, 89, 100, 40])
+  expect(res.reduce((acc, cur) => acc * cur)).toEqual(6818112000)
 })
--- src/2020/day3.ts
+++ src/2020/day3.ts
@@ -1,12 +1,17 @@
+export type Slope = [number, number]
+
 export function treesOnSlope(
   position: number,
-  [slopeX, slopeY]: [number, number],
+  [slopeX, slopeY]: Slope,
   map: string[],
 ): number {
   const currentLine = map.shift()
   if (!currentLine) return 0
 
   const tree = currentLine[position] === '#' ? 1 : 0
+
+  // move down once more
+  if (slopeY === 2) map.shift()
 
   const nowPos = (position + slopeX) % currentLine.length
   return tree + treesOnSlope(nowPos, [slopeX, slopeY], map)
