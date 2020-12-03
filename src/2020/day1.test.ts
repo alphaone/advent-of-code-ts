b0aab017c403a486c9f08564fcff585236fe7e8d
Torsten Mangner <tmangner@inoxio.de>
2020-12-03 14:33:02 +0100

    Day 1 Part 2

--- src/2020/day1.test.ts
+++ src/2020/day1.test.ts
@@ -1,4 +1,11 @@
-import { pairs, findPair, productOfPairWithSum } from './day1'
+import {
+  pairs,
+  findPair,
+  productOfPairWithSum,
+  triples,
+  findTriple,
+  productOfTripleWithSum,
+} from './day1'
 import { readNumberInput } from '../utils'
 
 test('solve puzzle a', () => {
@@ -34,6 +41,48 @@ describe('pairs', () => {
       [1, 2],
       [1, 3],
       [2, 3],
+    ])
+  })
+})
+
+test('solve puzzle b', () => {
+  const input = readNumberInput('src/2020/day1.txt')
+  expect(productOfTripleWithSum(2020, input)).toEqual(274879808)
+})
+
+test('find product of triple with sum 2020', () => {
+  expect(
+    productOfTripleWithSum(2020, [1721, 979, 366, 299, 675, 1456]),
+  ).toEqual(241861950)
+})
+
+test('find triple with sum of 2020', () => {
+  expect(findTriple(2020, [1721, 979, 366, 299, 675, 1456])).toEqual([
+    979,
+    366,
+    675,
+  ])
+})
+
+describe('triples', () => {
+  test('empty', () => {
+    expect(triples([])).toEqual([])
+  })
+
+  test('only two things', () => {
+    expect(triples([1, 2])).toEqual([])
+  })
+
+  test('simple triple', () => {
+    expect(triples([1, 2, 3])).toEqual([[1, 2, 3]])
+  })
+
+  test('get all triples', () => {
+    expect(triples([1, 2, 3, 4])).toEqual([
+      [1, 2, 3],
+      [1, 2, 4],
+      [1, 3, 4],
+      [2, 3, 4],
     ])
   })
 })
--- src/2020/day1.ts
+++ src/2020/day1.ts
@@ -1,4 +1,5 @@
 export type Pair = [number, number]
+export type Triple = [number, number, number]
 
 export function productOfPairWithSum(
   targetSum: number,
@@ -12,6 +13,18 @@ export function productOfPairWithSum(
   return x * y
 }
 
+export function productOfTripleWithSum(
+  targetSum: number,
+  entries: number[],
+): number {
+  const triple = findTriple(targetSum, entries)
+
+  if (!triple) return -1
+
+  const [x, y, z] = triple
+  return x * y * z
+}
+
 export function findPair(
   targetSum: number,
   entries: number[],
@@ -20,10 +33,28 @@ export function findPair(
   return pairs(entries).find(pairOfTargetSum)
 }
 
+export function findTriple(
+  targetSum: number,
+  entries: number[],
+): Triple | undefined {
+  let tripleOfTargetSum = ([x, y, z]: Triple) => x + y + z === targetSum
+  return triples(entries).find(tripleOfTargetSum)
+}
+
 export function pairs(entries: number[]): Pair[] {
   if (entries.length < 2) return []
   const current = entries.shift() as number
   return entries
     .map<Pair>((y) => [current, y])
     .concat(pairs(entries))
+}
+
+export function triples(entries: number[]): Triple[] {
+  if (entries.length < 3) return []
+
+  const current = entries.shift() as number
+  const pairsWithRest = pairs([...entries])
+  return pairsWithRest
+    .map<Triple>((p) => [current, ...p])
+    .concat(triples(entries))
 }
