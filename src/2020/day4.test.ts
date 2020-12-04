6a1b528afd74af6d53307beb5ecba3fda51652b2
Torsten Mangner <tmangner@inoxio.de>
2020-12-04 09:35:17 +0100

    Day 4 Part 2

--- src/2020/day4.test.ts
+++ src/2020/day4.test.ts
@@ -1,6 +1,19 @@
-import { parsePassport, Passport, validPassport } from './day4'
+import {
+  parsePassport,
+  Passport,
+  validByr,
+  validEcl,
+  validEyr,
+  validHcl,
+  validHgt,
+  validIyr,
+  validPassport,
+  validPassportB,
+  validPid,
+} from './day4'
 import { readInput } from '../utils'
 import * as fs from 'fs'
+import exp from 'constants'
 
 function passportsFromInput(): Passport[] {
   return fs
@@ -55,4 +68,59 @@ test('invalid passport', () => {
 
 test('solve a', () => {
   expect(passportsFromInput().filter(validPassport).length).toEqual(182)
+})
+
+test('valid byr', () => {
+  expect(validByr({ byr: '2003' })).toBeFalsy()
+  expect(validByr({ byr: '2002' })).toBeTruthy()
+  expect(validByr({ byr: '1920' })).toBeTruthy()
+  expect(validByr({ byr: '1919' })).toBeFalsy()
+})
+
+test('valid iyr', () => {
+  expect(validIyr({ iyr: '2021' })).toBeFalsy()
+  expect(validIyr({ iyr: '2020' })).toBeTruthy()
+  expect(validIyr({ iyr: '2010' })).toBeTruthy()
+  expect(validIyr({ iyr: '2009' })).toBeFalsy()
+})
+
+test('valid eyr', () => {
+  expect(validEyr({ eyr: '2031' })).toBeFalsy()
+  expect(validEyr({ eyr: '2030' })).toBeTruthy()
+  expect(validEyr({ eyr: '2020' })).toBeTruthy()
+  expect(validEyr({ eyr: '2019' })).toBeFalsy()
+})
+
+test('valid pid', () => {
+  expect(validPid({ pid: '000111222' })).toBeTruthy()
+  expect(validPid({ pid: '12345678' })).toBeFalsy()
+  expect(validPid({ pid: '1234567890' })).toBeFalsy()
+})
+
+test('valid ecl', () => {
+  expect(validEcl({ ecl: 'amb' })).toBeTruthy()
+  expect(validEcl({ ecl: 'wrong' })).toBeFalsy()
+  expect(validEcl({ ecl: 'hamb' })).toBeFalsy()
+})
+
+test('valid hcl', () => {
+  expect(validHcl({ hcl: '#00ff00' })).toBeTruthy()
+  expect(validHcl({ hcl: '#888' })).toBeFalsy()
+  expect(validHcl({ hcl: '00ff00' })).toBeFalsy()
+  expect(validHcl({ hcl: '#00ff00ff' })).toBeFalsy()
+})
+
+test('valid hgt', () => {
+  expect(validHgt({ hgt: '149cm' })).toBeFalsy()
+  expect(validHgt({ hgt: '150cm' })).toBeTruthy()
+  expect(validHgt({ hgt: '193cm' })).toBeTruthy()
+  expect(validHgt({ hgt: '194cm' })).toBeFalsy()
+  expect(validHgt({ hgt: '58in' })).toBeFalsy()
+  expect(validHgt({ hgt: '59in' })).toBeTruthy()
+  expect(validHgt({ hgt: '76in' })).toBeTruthy()
+  expect(validHgt({ hgt: '77in' })).toBeFalsy()
+})
+
+test('solve a', () => {
+  expect(passportsFromInput().filter(validPassportB).length).toEqual(109)
 })
--- src/2020/day4.ts
+++ src/2020/day4.ts
@@ -28,3 +28,61 @@ export function validPassport(passport: Passport): boolean {
   const keys = Object.keys(passport)
   return neededKeys.every((k) => keys.includes(k))
 }
+
+export function validPassportB(passport: Passport): boolean {
+  return (
+    validByr(passport) &&
+    validIyr(passport) &&
+    validEyr(passport) &&
+    validPid(passport) &&
+    validEcl(passport) &&
+    validHcl(passport) &&
+    validHgt(passport)
+  )
+}
+
+export function validByr(passport: Passport): boolean {
+  if (!passport.byr) return false
+  const byr = parseInt(passport.byr)
+  return byr >= 1920 && byr <= 2002
+}
+
+export function validIyr(passport: Passport): boolean {
+  if (!passport.iyr) return false
+  const iyr = parseInt(passport.iyr)
+  return iyr >= 2010 && iyr <= 2020
+}
+
+export function validEyr(passport: Passport): boolean {
+  if (!passport.eyr) return false
+  const eyr = parseInt(passport.eyr)
+  return eyr >= 2020 && eyr <= 2030
+}
+
+export function validPid(passport: Passport): boolean {
+  if (!passport.pid) return false
+  return !!passport.pid.match(/^\d{9}$/)
+}
+
+export function validEcl(passport: Passport): boolean {
+  if (!passport.ecl) return false
+  return !!passport.ecl.match(/^(amb|blu|brn|gry|grn|hzl|oth)$/)
+}
+
+export function validHcl(passport: Passport): boolean {
+  if (!passport.hcl) return false
+  return !!passport.hcl.match(/^#([0-9a-f]){6}$/)
+}
+
+export function validHgt(passport: Passport): boolean {
+  if (!passport.hgt) return false
+  if (!!passport.hgt.match(/cm$/)) {
+    const hgt = parseInt(passport.hgt)
+    return hgt >= 150 && hgt <= 193
+  }
+  if (!!passport.hgt.match(/in$/)) {
+    const hgt = parseInt(passport.hgt)
+    return hgt >= 59 && hgt <= 76
+  }
+  return false
+}
