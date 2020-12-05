214bee8fc4a5b531b950ce3d8c563fa8a704f0a9
Torsten Mangner <tmangner@inoxio.de>
2020-12-05 11:44:15 +0100

    Day 5 Part 2

--- src/2020/day5.test.ts
+++ src/2020/day5.test.ts
@@ -1,19 +1,5 @@
-import { parseCol, parseRow, seatId } from './day5'
+import { range, seatId } from './day5'
 import { readInput } from '../utils'
-
-test('parse row', () => {
-  expect(parseRow('FBFBBFFRLR')).toEqual(44)
-  expect(parseRow('BFFFBBFRRR')).toEqual(70)
-  expect(parseRow('FFFBBBFRRR')).toEqual(14)
-  expect(parseRow('BBFFBBFRLL')).toEqual(102)
-})
-
-test('parse column', () => {
-  expect(parseCol('FBFBBFFRLR')).toEqual(5)
-  expect(parseCol('BFFFBBFRRR')).toEqual(7)
-  expect(parseCol('FFFBBBFRRR')).toEqual(7)
-  expect(parseCol('BBFFBBFRLL')).toEqual(4)
-})
 
 test('seat id', () => {
   expect(seatId('BFFFBBFRRR')).toEqual(567)
@@ -25,4 +11,16 @@ test('solve a', () => {
   const lines = readInput('src/2020/day5.txt').slice(0, -1)
   const ids = lines.map((l) => seatId(l))
   expect(Math.max(...ids)).toEqual(858)
+})
+
+test('range', () => {
+  expect(range(2, 5)).toEqual([2, 3, 4])
+})
+
+test('solve b', () => {
+  const lines = readInput('src/2020/day5.txt').slice(0, -1)
+  const ids = lines.map((l) => seatId(l))
+  const allSeats = range(Math.min(...ids), Math.max(...ids) + 1)
+  const freeSeats = allSeats.filter((x) => !ids.includes(x))
+  expect(freeSeats).toEqual([557])
 })
--- src/2020/day5.ts
+++ src/2020/day5.ts
@@ -1,19 +1,12 @@
-export function parseRow(input: string): number {
+export function seatId(input: string): number {
   const inputAsBinaryString = input
-    .substr(0, 7)
     .replace(/F/g, '0')
     .replace(/B/g, '1')
-  return parseInt(inputAsBinaryString, 2)
-}
-
-export function parseCol(input: string): number {
-  const inputAsBinaryString = input
-    .substr(7)
     .replace(/L/g, '0')
     .replace(/R/g, '1')
   return parseInt(inputAsBinaryString, 2)
 }
 
-export function seatId(input: string): number {
-  return parseRow(input) * 8 + parseCol(input)
+export function range(start: number, end = 0) {
+  return [...Array(end - start).keys()].map((i) => i + start)
 }
