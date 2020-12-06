26df3caf297918cbc982e93c57fa70f9b17b7a9f
Torsten Mangner <tmangner@inoxio.de>
2020-12-06 13:48:17 +0100

    Day 6 Part 2

--- src/2020/day6.test.ts
+++ src/2020/day6.test.ts
@@ -1,8 +1,17 @@
-import { countAnswers, inputToGroups } from './day6'
+import {
+  countAnswersIntersection,
+  countAnswersUnion,
+  inputToGroups,
+  intersection,
+} from './day6'
 import * as fs from 'fs'
 
 test('count answers', () => {
-  expect(countAnswers(['abcx', 'abcy', 'abcz'])).toBe(6)
+  expect(countAnswersUnion(['abcx', 'abcy', 'abcz'])).toBe(6)
+})
+
+test('count answers intersection', () => {
+  expect(countAnswersIntersection(['abcx', 'abcy', 'abcz'])).toBe(3)
 })
 
 const exampleInput = 'abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb'
@@ -21,7 +30,7 @@ test('input to groups', () => {
 test('solve example', () => {
   expect(
     inputToGroups(exampleInput)
-      .map(countAnswers)
+      .map(countAnswersUnion)
       .reduce((sum, a) => sum + a),
   ).toEqual(11)
 })
@@ -29,7 +38,33 @@ test('solve example', () => {
 test('solve a', () => {
   expect(
     inputToGroups(input)
-      .map(countAnswers)
+      .map(countAnswersUnion)
       .reduce((sum, a) => sum + a),
   ).toEqual(6443)
+})
+
+test('intersection', () => {
+  expect(intersection([new Set([1, 2, 3])])).toEqual(new Set([1, 2, 3]))
+  expect(intersection([new Set([1, 2, 3]), new Set([1, 2, 4])])).toEqual(
+    new Set([1, 2]),
+  )
+  expect(
+    intersection([new Set([1, 2, 3]), new Set([1, 2, 4]), new Set([1, 3, 4])]),
+  ).toEqual(new Set([1]))
+})
+
+test('solve example intersection', () => {
+  expect(
+    inputToGroups(exampleInput)
+      .map(countAnswersIntersection)
+      .reduce((sum, a) => sum + a),
+  ).toEqual(6)
+})
+
+test('solve b', () => {
+  expect(
+    inputToGroups(input)
+      .map(countAnswersIntersection)
+      .reduce((sum, a) => sum + a),
+  ).toEqual(3232)
 })
--- src/2020/day6.ts
+++ src/2020/day6.ts
@@ -1,8 +1,24 @@
-export function countAnswers(group: string[]) {
+export function countAnswersUnion(group: string[]) {
   const answers = group.flatMap((person) => person.split(''))
   return new Set(answers).size
 }
 
+export function intersection([first, second, ...rest]: Set<any>[]): Set<any> {
+  if (!first) return new Set<any>()
+  if (!second) return first
+
+  const intersectionBetweenFirstAndSecond = new Set(
+    [...first].filter((x) => second.has(x)),
+  )
+  return intersection([intersectionBetweenFirstAndSecond, ...rest])
+}
+
+export function countAnswersIntersection(group: string[]) {
+  const answers = group.map((person) => new Set(person.split('')))
+
+  return intersection(answers).size
+}
+
 export function inputToGroups(input: string): string[][] {
-  return input.split(/\n\n/).map((group) => group.split(/\n/))
+  return input.split(/\n\n/).map((group) => group.trim().split(/\n/))
 }
